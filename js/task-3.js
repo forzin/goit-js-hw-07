5df96f1204aba9a512373b4c31fa4321ef5527b6
Danylo Korobko <dakorobko@gmail.com>
2024-06-24 16:42:52 +0200

    change

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,20 @@
-function checkForSpam(message) {
-   
-    if (message.toLowerCase().includes("sale") || message.toLowerCase().includes("spam")) {
-        message = true;
-    } else {
-        message = false;
+
+
+function filterArray(numbers, value) {
+
+    let newMasiv1 = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+            newMasiv1.push(numbers[i]);
+        }
     }
 
-    return message;
+    return newMasiv1;
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
